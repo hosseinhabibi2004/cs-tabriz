594f95d83da8be11a37d2b7a304640c0e22a4824
x_o <hosseinhabibi2004@gmail.com>
2023-09-19 22:51:46 +0330

    [refactor] TGUser Model
    - [feat] add `__str__` function

--- app/apps/core/models.py
+++ app/apps/core/models.py
@@ -2,15 +2,24 @@ from django.db import models
 
 
 class TGUser(models.Model):
-    id = models.BigIntegerField(primary_key=True, verbose_name="Telegram User ID")
+    class Meta:
+        db_table = "tg_user"
+
+    id = models.BigIntegerField(
+        primary_key=True,
+        verbose_name="Telegram ID",
+    )
     full_name = models.CharField(
-        max_length=64, blank=True, verbose_name="Telegram User Full Name"
+        max_length=64,
+        verbose_name="Full Name",
     )
     username = models.CharField(
-        max_length=64, null=True, verbose_name="Telegram Username"
+        max_length=64,
+        null=True,
+        verbose_name="Telegram Username",
     )
 
     objects: models.manager.BaseManager["TGUser"]
 
-    class Meta:
-        db_table = "tg_user"
+    def __str__(self) -> str:
+        return f"{self.full_name}"
